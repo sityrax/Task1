efd071860745bf2f117aa71bcc71833800dbfdea
Gugnin Dmitry <76938322+sityrax@users.noreply.github.com>
2021-12-20 03:43:59 +0500

    Task1
    First commit

---
+++ Byndyusoft/Task.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Byndyusoft
+{
+    public class Task
+    {
+        //Напишите функцию, на вход которой приходит массив чисел.Функция возвращает сумму двух минимальных элементов массива.
+        //Например, если дан массив[4, 0, 3, 19, 492, -10, 1], то результатом будет -10, потому что два минимальных числа -10 и 0, а их сумма -10.
+        //Напишите минимум 3 модульных теста на эту функцию.
+        //HINT: учти, что массив может быть пустым, или без цифр или состоять из 100 млн.элементов, поэтому надо учесть разные граничные условия.
+
+        public static int Sum(params int[] numbers)  //Если диапазон допустимых значений ± 2 147 483 647.
+        {
+            try
+            {
+                if (numbers.Length > 1)
+                {
+                    int first = int.MaxValue;
+                    int second = int.MaxValue;
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (first >= numbers[i])
+                        {
+                            second = first;
+                            first = numbers[i];
+                        }
+                    }
+                    checked
+                    {
+                        return first + second;
+                    }
+                }
+                throw new IndexOutOfRangeException("Array size must be > 2");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                throw;  //Какое-либо поведение в случае неполного массива (в массиве меньше 2 элементов).
+            }
+            catch (NullReferenceException)
+            {
+                throw;  //Какое-либо поведение в случае пустой ссылки (...или пустой массив).
+            }
+            catch (OverflowException)
+            {
+                throw;  //Какое-либо поведение в случае переполнения (100 млн.элементов).
+            }
+            catch (Exception)
+            {
+                throw;  //Поведение во всех остальных случаях.
+            }
+        }
+    }
+}
---
+++ ByndyusoftTests/TaskTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Byndyusoft;
+using System;
+
+
+namespace Byndyusoft.Tests
+{
+    [TestClass()]
+    public class TaskTests
+    {
+        [TestMethod()]
+        public void Sum_base()
+        {
+            //arrange
+            int[] input = new[] { 4, 0, 3, 19, 492, -10, 1 };
+            int expected = -10;
+
+            //act
+            int actual = Task.Sum(input);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void Sum_0and0_0returned()
+        {
+            //arrange
+            int[] input = new int[2] { 0, 0 };
+            int expected = 0;
+
+            //act
+            int actual = Task.Sum(input);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [TestMethod()]
+        public void Sum_IndexOutOfRangeException_zeroValue()
+        {
+            int actual = Task.Sum(0);
+        }
+
+
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [TestMethod()]
+        public void Sum_IndexOutOfRangeException_emptyArray()
+        {
+            int actual = Task.Sum(new int[0]);
+        }
+
+
+        [ExpectedException(typeof(OverflowException))]
+        [TestMethod()]
+        public void Sum_OverflowException_value()
+        {
+            int actual = Task.Sum(new int[] { int.MaxValue, int.MaxValue });
+        }
+
+
+        [ExpectedException(typeof(OverflowException))]
+        [TestMethod()]
+        public void Sum_OverflowException_range()
+        {
+            int actual = Task.Sum(new int[ulong.MaxValue]);
+        }
+
+
+        [ExpectedException(typeof(NullReferenceException))]
+        [TestMethod()]
+        public void Sum_NullReferenceException()
+        {
+            int actual = Task.Sum(null);
+        }
+    }
+}
